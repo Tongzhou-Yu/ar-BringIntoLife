db0922ba371b84d872e5290ee58166588f885b68
Tongzhou-Yu <tongzhou.yu@network.rca.ac.uk>
2020-09-20 01:54:00 +0800

    Add AR Plane Changer function

---
+++ ar-BringIntoLife/Assets/Scripts/ARPlaneChanger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARPlaneChanger : MonoBehaviour
+{
+
+public void ChangePlane(Material PlaneMaterial)
+    {
+        GetComponentInChildren<MeshRenderer>().material = PlaneMaterial;
+    }
+}
